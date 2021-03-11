31d42d6e86919a1263a33ed262be2f132f45db7f
Balou9 <emmanuel.owosekun@gmail.com>
2021-03-11 21:24:36 +0100

    ci: adds ci workflow

--- ecs_opt_ami.sh
+++ ecs_opt_ami.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 script="ecs-ami"
 
-function example {
-    echo -e "example: $script -var amazon-linux-2 -ver recommended -it VAL"
+example () {
+    echo -e "example: $script -var amazon-linux-2"
 }
 
-function usage {
+usage () {
     echo -e "usage: $script --variant [--version] [--instanceType]"
 }
 
-function help {
+help () {
   usage
     echo -e "MANDATORY:"
-    echo -e "  -var, --variant  VAL  Amazon ECS-optimized AMI variant"
+    echo -e "  -var, --variant        Amazon ECS-optimized AMI variant"
     echo -e "OPTION:"
-    echo -e "  -ver, --version  VAL  Amazon ECS-optimized AMI version\n"
-    echo -e "  -it, --instanceType  VAL  Amazon EC2 instance type \n"
-    echo -e "  -h,  --help             Prints this help\n"
+    echo -e "  -ver, --version        Amazon ECS-optimized AMI version\n"
+    echo -e "  -it,  --instanceType   Amazon EC2 instance type \n"
+    echo -e "  -h,   --help           Prints this help\n"
   example
 }
 
@@ -24,3 +24,5 @@ function help {
 VARIANT=
 VERSION="recommended"
 INSTANCE_TYPE=
+
+help
