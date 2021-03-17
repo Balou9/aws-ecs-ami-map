7d95983cbdc93e74ab7038187f76ddc93e604630
Balou9 <emmanuel.owosekun@gmail.com>
2021-03-17 23:27:56 +0100

    chore:

--- ecs_opt_ami.sh
+++ ecs_opt_ami.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
-script="ecs-ami"
-mandatory_args=1
-example () {
-    echo -e "example: $script -var amazon-linux-2"
-}
+script="ecsami"
+VARIANT=$1
 
-usage () {
-    echo -e "usage: $script --variant [--version] [--instanceType]"
-}
-
-help () {
-  usage
-    echo -e "MANDATORY:"
-    echo -e "  -var, --variant        Amazon ECS-optimized AMI variant"
-    echo -e "OPTION:"
-    echo -e "  -ver, --version        Amazon ECS-optimized AMI version\n"
-    echo -e "  -it,  --instanceType   Amazon EC2 instance type \n"
-    echo -e "  -h,   --help           Prints this help\n"
-  example
-}
-
-
-VARIANT=
-VERSION="recommended"
-INSTANCE_TYPE=
+aws ssm get-parameters --names "/aws/service/ecs/optimized-ami/$1/recommended"
