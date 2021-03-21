8f801b2313a5a674e7920c6ed2f596f2929b94e4
Balou9 <emmanuel.owosekun@gmail.com>
2021-03-21 10:22:32 +0100

    chore: bashsplit

--- ecs_opt_ami.sh
+++ ecs_opt_ami.sh
@@ -2,4 +2,25 @@
 script="ecsami"
 VARIANT=$1
 
-aws ssm get-parameters --names "/aws/service/ecs/optimized-ami/$1/recommended"
+IFS='\:"|,\:' read -ra payload <<< $(aws ssm get-parameters \
+  --names "/aws/service/ecs/optimized-ami/$1/recommended" \
+  | jq ".Parameters|.[0]|.Value")
+
+# get_image_id(param) {
+#
+# }
+#
+# get_os(param) {
+#
+# }
+
+for i in "${payload[@]}"
+do
+  if [[ $i != "" ]]
+  then
+    echo $i
+  fi
+done
+
+aws ssm get-parameters \
+  --names "/aws/service/ecs/optimized-ami/$1/recommended"
