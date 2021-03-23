bbd718de9bfe84d9186105a82a5e29ada8bfc2b3
Balou9 <emmanuel.owosekun@gmail.com>
2021-03-23 17:31:40 +0100

    ci: run test

--- ecs_opt_ami.sh
+++ ecs_opt_ami.sh
@@ -10,6 +10,6 @@ for (( i = 0; i < ${#payload[*]}; ++ i ))
 do
   if [[ $i != "" ]] && echo ${payload[$i]} | grep -Eq "image_id|os"
   then
-    echo "${payload[$i]}" "${payload[$i+5]}"
+    echo "${payload[$i]}:" "${payload[$i+5]}"
   fi
 done
