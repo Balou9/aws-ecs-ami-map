715b79dffc95ce9dc439c141647c2452db68e840
Balou9 <emmanuel.owosekun@gmail.com>
2021-10-01 19:17:06 +0200

    chore: cleaning up back at it

--- ecs_opt_ami.sh
+++ ecs_opt_ami.sh
@@ -2,14 +2,5 @@
 script="ecsami"
 VARIANT=$1
 
-IFS='\:"|,\:' read -ra payload <<< $(aws ssm get-parameters \
-  --names "/aws/service/ecs/optimized-ami/$1/recommended" \
-  | jq ".Parameters|.[0]|.Value")
-
-for (( i = 0; i < ${#payload[*]}; ++ i ))
-do
-  if [[ $i != "" ]] && echo ${payload[$i]} | grep -Eq "image_id|os"
-  then
-    echo "${payload[$i]}:" "${payload[$i+5]}"
-  fi
-done
+strparam="/aws/service/ecs/optimized-ami/$VARIANT/recommended/image_id"
+aws ssm get-parameters --names "$strparam" | jq ".Parameters|.[0]|.Value"
