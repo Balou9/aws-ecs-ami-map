a7acecac0d8b22f92a61a37a1ab20ba2c177762f
Balou9 <emmanuel.owosekun@gmail.com>
2021-10-02 21:31:19 +0200

    chore: gets os and image_id

--- ecs_opt_ami.sh
+++ ecs_opt_ami.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-script="ecsami"
-VARIANT=$1
+VARIANT="$1"
+PROC="$2"
 
-strparam="/aws/service/ecs/optimized-ami/$VARIANT/recommended/image_id"
-aws ssm get-parameters --names "$strparam" | jq ".Parameters|.[0]|.Value"
+create_image_id_string_param() {
+  if [[ -z "$2" ]]
+  then
+    image_id_param="/aws/service/ecs/optimized-ami/$VARIANT/recommended/image_id"
+  else
+    image_id_param="/aws/service/ecs/optimized-ami/$VARIANT/$PROC/recommended/image_id"
+  fi
+
+  echo $image_id_param
+}
+
+create_string_param_os () {
+  if [[ -z "$2" ]]
+  then
+    os_param="/aws/service/ecs/optimized-ami/$VARIANT/recommended/os"
+  else
+    os_param="/aws/service/ecs/optimized-ami/$VARIANT/$PROC/recommended/os"
+  fi
+
+  echo $os_param
+}
+
+IMAGE_ID_PARAM=$(create_image_id_string_param $VARIANT)
+OS_PARAM=$(create_string_param_os $VARIANT)
+
+aws ssm get-parameters --names "$OS_PARAM" | jq ".Parameters|.[0]|.Value"
+aws ssm get-parameters --names "$IMAGE_ID_PARAM" | jq ".Parameters|.[0]|.Value"
